ab4738a369168504735904ed5f69dfee1465a466
Mohan <mohanchhalotre@gmail.com>
2023-11-08 17:10:38 +0530

    add admin credentials

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,4 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rake ffcrm:setup:admin USERNAME=peramali PASSWORD=peramali@123 email=admin@example.com
